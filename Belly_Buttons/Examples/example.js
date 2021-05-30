31da2c2c281335fd6a10372698c8d1ca07643431
Scott Hammel <scott_hammel@icloud.com>
2021-05-30 14:42:05 -0500

    added belly button data.

---
+++ Belly_Buttons/Examples/example.js
@@ -0,0 +1,77 @@
+// Sort by Population
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var Pop = cities.map(function(population){
+    return population.population;
+});
+console.log(Pop);
+
+
+// Sort by City Name
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityNames = cities.map(function(city){
+    return city.City;
+});
+console.log(cityNames);
+
+
+// The filter() Method
+// It returns an array of integers that are larger than 1:
+var numbers = [1,2,3,4,5];
+
+var larger = numbers.filter(function(num){
+    return num > 1;
+});
+
+console.log(larger);
+
+
+// The slice() Method
+// Use slice() to select the first three elements of the words array
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+words.slice(3, );
